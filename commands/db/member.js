6bb4596ee83cb63af5199c946a093e7a8a29103a
Hazel Ann Hernandez <halh2012@shaw.ca>
2018-05-08 12:38:08 -0700

    added showInfo function

--- commands/db/member.js
+++ commands/db/member.js
@@ -12,5 +12,18 @@ module.exports = {
     let res = 'Added to database. Welcome to the group ' + user + '!';
 
     return res;
+  },
+
+  myInfo: function(msg, con) {
+    let id = msg.author.id;
+
+    let sql = 'SELECT * FROM member WHERE memberID = "' + id + '"';
+
+    con.query(sql, (err, rows) => {
+      let user = rows[0].name;
+      let num = rows[0].marbles;
+
+      msg.channel.send('Username: ' + user + '\nMarbles: ' + num);
+    })
   }
 };
