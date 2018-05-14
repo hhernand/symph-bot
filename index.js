0987d95197f8d251bb1da8e2e29964f19550b1a3
Hazel Ann Hernandez <halh2012@shaw.ca>
2018-05-14 14:42:52 -0700

    new index file

---
+++ index.js
@@ -0,0 +1,41 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const mysql = require('mysql');
+
+const db = require('require-dir-all')('./commands/db');
+
+var con;
+if(process.env.JAWSDB_URL) {
+    con = mysql.createConnection(process.env.JAWSDB_URL);
+};
+
+let res = '';
+
+bot.on('message', (message) => {
+  if (message.content == 'Hello!') {
+    message.channel.send('Hi ' + message.author.username + '!');
+  }
+
+  if (message.content.startsWith('!add')) {
+    res = db.member.addMember(message, con);
+    message.channel.send(res);
+  }
+
+  if (message.content.startsWith('!rewardMarbles')) {
+    db.marbles.rewardMarbles(message, con);
+  }
+
+  if (message.content == '!myInfo') {
+    db.member.myInfo(message, Discord, con);
+  }
+
+  if (message.content == '!shop') {
+    db.marbles.shopList(message, Discord, con);
+  }
+
+  if (message.content.startsWith('!buy')) {
+    db.marbles.buy(message, con);
+  }
+});
+
+bot.login(process.env.BOT_TOKEN);
