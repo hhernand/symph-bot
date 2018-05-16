e82497751572e0c2ac2bef55ee55c915410f0e2c
Hazel Ann Hernandez <halh2012@shaw.ca>
2018-05-16 11:40:42 -0700

    added check specific user function

--- commands/db/member.js
+++ commands/db/member.js
@@ -27,47 +27,97 @@ module.exports = {
     let id = msg.author.id;
 
     access.memberByID(id, con, function(member) {
-      let user = member[0].name;
-      let num = member[0].marbles;
+      if (member.length == 1) {
+        let user = member[0].name;
+        let num = member[0].marbles;
 
-      access.owns(id, con, function(owned) {
-        if (owned.length == 0) {
-          const embed = new ds.RichEmbed()
-            .setTitle(msg.author.username)
-            .setColor('BLUE')
-            .setThumbnail(msg.author.avatarURL)
-            .addField('DA Username', user, true)
-            .addField('Marbles', num, true);
-
-          msg.channel.send(embed);
-        }
-        else {
-          var res = '';
-          var q = '';
-
-          async.eachSeries(owned, function(el, callback) {
-
-            access.itemByID(el.itemID, con, function(entry) {
-              if (el.quantity > 1) q = ' x ' + String(el.quantity);
-              res += entry[0].name + q + '\n';
-              callback();
-            });
-
-            q = '';
-          },
-          function(err, owned) {
+        access.owns(id, con, function(owned) {
+          if (owned.length == 0) {
             const embed = new ds.RichEmbed()
               .setTitle(msg.author.username)
               .setColor('BLUE')
               .setThumbnail(msg.author.avatarURL)
               .addField('DA Username', user, true)
-              .addField('Marbles', num, true)
-              .addField('Items', res);
+              .addField('Marbles', num, true);
 
             msg.channel.send(embed);
-          });
-        }
-      });
+          }
+          else {
+            var res = '';
+            var q = '';
+
+            async.eachSeries(owned, function(el, callback) {
+
+              access.itemByID(el.itemID, con, function(entry) {
+                if (el.quantity > 1) q = ' x ' + String(el.quantity);
+                res += entry[0].name + q + '\n';
+                callback();
+              });
+
+              q = '';
+            },
+            function(err, owned) {
+              const embed = new ds.RichEmbed()
+                .setTitle(msg.author.username)
+                .setColor('BLUE')
+                .setThumbnail(msg.author.avatarURL)
+                .addField('DA Username', user, true)
+                .addField('Marbles', num, true)
+                .addField('Items', res);
+
+              msg.channel.send(embed);
+            });
+          }
+        });
+      }
+    });
+  },
+
+  checkMember: function(msg, ds, con) {
+    let user = msg.content.split(" ")[1];
+
+    access.memberByName(user, con, function(member) {
+      if (member.length == 1) {
+        let id = member[0].memberID;
+        let num = member[0].marbles;
+
+        access.owns(id, con, function(owned) {
+          if (owned.length == 0) {
+            const embed = new ds.RichEmbed()
+              .setTitle('Member Information')
+              .setColor('BLUE')
+              .addField('DA Username', user, true)
+              .addField('Marbles', num, true);
+
+            msg.channel.send(embed);
+          }
+          else {
+            var res = '';
+            var q = '';
+
+            async.eachSeries(owned, function(el, callback) {
+
+              access.itemByID(el.itemID, con, function(entry) {
+                if (el.quantity > 1) q = ' x ' + String(el.quantity);
+                res += entry[0].name + q + '\n';
+                callback();
+              });
+
+              q = '';
+            },
+            function(err, owned) {
+              const embed = new ds.RichEmbed()
+                .setTitle('Member Information')
+                .setColor('BLUE')
+                .addField('DA Username', user, true)
+                .addField('Marbles', num, true)
+                .addField('Items', res);
+
+              msg.channel.send(embed);
+            });
+          }
+        });
+      }
     });
   }
 };
