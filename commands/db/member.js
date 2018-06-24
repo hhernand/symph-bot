ef035b11816225d19f57f046f883d9cc75c5ce30
Hazel Hernandez <halh2012@gmail.com>
2018-06-24 11:40:09 -0700

    claim for june 24

--- commands/db/member.js
+++ commands/db/member.js
@@ -200,6 +200,13 @@ module.exports = {
 
               res = msg.author + ' You claimed a Texture Change Bath Bomb!';
             }
+            else if (num == 24) {
+              marble.grantMarbles(id, 5, con);
+              let entry = 'INSERT INTO claimed VALUES ("' + id + '", ' + num + ')';
+              con.query(entry);
+
+              res = msg.author + ' You claimed 5 marbles!';
+            }
             else {
               res = msg.author + ' That\'s not a valid claim! Maybe you got the wrong number?';
             }
