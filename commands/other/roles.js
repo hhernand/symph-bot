c00837c9784cd5b8a92339338937d789b966e374
Hazel Hernandez <halh2012@gmail.com>
2019-02-16 14:48:54 -0800

    arpg and gamecorner roles

--- commands/other/roles.js
+++ commands/other/roles.js
@@ -1,23 +1,24 @@
 module.exports = {
     assign: function (msg) {
         let type = msg.content.split(' ')[1];
-        if (type == 'streams') {
-            msg.member.addRole('494544240789946388');
-            msg.channel.send(msg.author + ' You now have the streams role!');
+        let roles = {
+            streams: {
+                id: '494544240789946388',
+                message: 'You now have the streams role! You\'ll get pinged when anyone\'s hosting a stream.'
+            },
+            arpgnews: {
+                id: '546412364904398848',
+                message: 'You\'ve signed up to be pinged for any ARPG news we post!'
+            },
+            gamecorner: {
+                id: '546412451638411284',
+                message: 'You\'ll now be alerted when we host games in the server!'
+            }
         }
-        else if (type == 'vampires') {
-            msg.member.addRole('496024106790289418');
-            msg.channel.send(msg.author + ' You joined team vampires!');
-        }
-        else if (type == 'werewolves') {
-            msg.member.addRole('496024187505475629');
-            msg.channel.send(msg.author + ' You joined team werewolves!');
-        }
-        else if (type == 'zombies') {
-            msg.member.addRole('496024360390230048');
-            msg.channel.send(msg.author + ' You joined team zombies!');
-        }
-        else {
+        if (type in roles ) {
+            msg.member.addRole(roles[type].id);
+            msg.channel.send( `${msg.author} ${roles[type].message}`);
+        } else {
             msg.channel.send(msg.author + ' That role doesn\'t exist or you can\'t give yourself that role!');
         }
     }
